df9f1bd3e4574bc307ad2640750f0b28265c6cde
Mason Seaquist <mason.seaquist@headwaters.org>
2023-01-19 15:56:46 -0600

    Create Unit 1.2 math and logic.py

---
+++ Unit 1.2 math and logic.py
@@ -0,0 +1,23 @@
+print(.8 ** 2)
+
+#Logic
+#An and statement will only print true if there 
+#<, >, ==, !=, <=, >=
+#Math
+#+,-,*,/ are normal. The only difference is / returns a float\
+#Division
+#Regular division /
+#loor division //
+#Floors the number
+#Modulo %
+#gives the remainder
+#Powers
+#Use **
+#Order of operations is used
+
+
+x = 2
+
+print(x == 2 and x != 1)
+print(x==3 or x==2)
+
