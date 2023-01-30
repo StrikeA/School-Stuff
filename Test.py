9c89730a89a4fced96243f330498a0b7585be68b
Mason Seaquist <mason.seaquist@headwaters.org>
2023-01-30 15:22:42 -0600

    Update Test.py

---
+++ Test.py
@@ -0,0 +1,23 @@
+# Write your python code below each comment
+
+# 1. Print “Hello World!” to the console
+print("Hello World")
+# 2. Write a multi-line comment
+"""hi """
+# 3. Write a single line comment
+# hello
+# 4. Declare a variable named ‘my_var’ to hold this string: “selur nohtyp”
+my_var = "selur nohtyp"
+# 5. Call the first item in the string my_var and print to the console
+print(my_var[0])
+# 6. Call the last item in the string my_var and print to the console
+print(my_var[11])
+# 7. Reverse the string using the string method and print to the console
+print(my_var[::-1])
+# 8. Declare a variable named ‘my_lst’ to hold this list: [‘first’, ‘second’, ‘third’]
+my_lst = ['first', 'second', 'third']
+# 9. Call the second item in the list and print to the console
+print(my_lst[1])
+# 10. Add the string ‘last’ to my_lst and print to the console
+my_lst.append('last')
+print(my_lst)
