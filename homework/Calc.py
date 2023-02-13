d02cb8495e47eaf4f7999b60b6071b0ad18dd518
Mason Seaquist <mason.seaquist@headwaters.org>
2023-02-13 15:38:18 -0600

    Create Calc.py

---
+++ homework/Calc.py
@@ -0,0 +1,24 @@
+def main():
+    calc = True
+    while calc:
+        number1 = float(input("What is your first number? "))
+        number2 = float(input("What is your second number? "))
+        opp = input("What operation would you like to use addition (+), subtraction (-), multiplication (*), or division (/)? ")
+        if opp == "+":
+            answer = number1 + number2
+        elif opp == "-":
+            answer = number1 - number2
+        elif opp == "*":
+            answer = number1 * number2
+        elif opp == "/":
+            answer = number1 / number2
+        else:
+            print("You selected an unsupported operation.")
+            break
+        print("The answer is:", answer)
+        calc_again = input("Would you like to calculate another number (y or n)? ")
+        if calc_again != "y":
+            print("Thank you for using this calculator.")
+            break
+
+main()
